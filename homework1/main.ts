bd41c133c86a1a5cfc99fb9c513403c80b037c67
Anastasia <anastasia@host-192-168-0-102.sohonet.ua>
2023-10-16 14:28:10 +0300

    Add homework 1

---
+++ homework1/main.ts
@@ -0,0 +1,130 @@
+class School {
+    directions: string[] = [];
+
+    addDirection(direction: string) {
+        this.directions.push(direction);
+    }
+}
+
+class Direction {
+    levels: number[] = [];
+    _name: string;
+
+    get name(): string {
+        return this._name;
+    }
+
+    constructor(name: string) {
+        this._name = name;
+    }
+
+    addLevel(level:number) {
+        this.levels.push(level);
+    }
+}
+
+
+class Level {
+    groups: any[] = [];
+    _name: string;
+    _program: string;
+
+    constructor(name:string, program: string) {
+        this._name = name;
+        this._program = program;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get program(): string {
+        return this._program;
+    }
+
+    addGroup(group): any {
+        this.groups.push(group);
+    }
+}
+
+class Group {
+    _students: any = [];
+    directionName: string;
+    levelName: string;
+
+    get students(): any {
+        return this._students;
+    }
+
+    constructor(directionName: string, levelName: string) {
+        this.directionName = directionName;
+        this.levelName = levelName;
+    }
+
+    addStudent(student): any {
+        this._students.push(student);
+    }
+
+    showPerformance(): any {
+        const sortedStudents: any = this.students.toSorted(
+            (a, b) => b.getPerformanceRating() - a.getPerformanceRating()
+        );
+
+
+        return sortedStudents;
+    }
+}
+
+class Student {
+    grades: any = {};
+    attendance: any = [];
+    firstName: string;
+    lastName: string;
+    birthYear: number;
+
+    constructor(firstName: string, lastName: string, birthYear: number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+    }
+
+    get fullName(): string {
+        return `${this.lastName} ${this.firstName}`;
+    }
+
+    set fullName(value:string) {
+        [this.lastName, this.firstName] = value.split(" ");
+    }
+
+    get age():number {
+        return new Date().getFullYear() - this.birthYear;
+    }
+
+    setGrade(subject: string, grade: number){
+        this.grades[subject] = grade;
+    }
+
+    markAttendance(present:boolean) {
+        this.attendance.push(present);
+    }
+
+    getPerformanceRating(): number {
+        const gradeValues = Object.values(this.grades);
+
+
+        if (gradeValues.length === 0) return 0;
+
+
+        const averageGrade : number =
+            gradeValues.reduce((sum, grade) => sum + grade, 0) / gradeValues.length;
+
+
+        const attendancePercentage: number =
+            (this.attendance.filter((present) => present).length /
+                this.attendance.length) *
+            100;
+
+
+        return (averageGrade + attendancePercentage) / 2;
+    }
+}
